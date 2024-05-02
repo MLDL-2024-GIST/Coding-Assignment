e3ff1ab9ba95fcde1ef6a699b51ace0c5b8cff07
ChnanChan <97542056+ChnanChan@users.noreply.github.com>
2024-05-02 13:35:30 +0900

    Update SVM_kernel.py

--- SVM_kernel.py
+++ SVM_kernel.py
@@ -4,5 +4,3 @@ from numpy import random
 
 dataset = sklearn.datasets.make_moons(n_samples = 300, noise = 0.3, random_state = 20) # you can change noise and random_state where noise >= 0.15
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.3, random_state = 100)
-
-class KSVM():
