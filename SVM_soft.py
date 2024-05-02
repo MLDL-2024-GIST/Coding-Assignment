75fd26fdd9e41ecb30bede34ad9ed8aac1716ffb
sk000514 <91325559+sk000514@users.noreply.github.com>
2024-05-02 15:58:33 +0900

    Update SVM_soft.py

--- SVM_soft.py
+++ SVM_soft.py
@@ -4,4 +4,14 @@ from numpy import random
 iris=datasets.load_iris()
 x=iris.data[50:,[2,3]]
 y=iris.target[50:]
+class SSVM:
+    def __init__(self,  ):
+        pass
+    def fit(self, X, y):
+        pass
+    def predict(self, X):
+        pass
+
+def computeClassificationAcc(gt_y,pred_y):
+    pass
 
